7ae7585bf2200d60e82d0a79c15de74dc3a2daef
nriccio <nicolasriccio78@gmail.com>
2020-09-15 21:17:21 +0200

    Maj todos localStorage v2

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-import React from "react"
-import Todos from "./components/Todos"
-import AppContainer from "./components/AppContainer"
-import ModeSwitch from "./components/ModeSwitch"
-import ModeContextProvider from "./context/ModeContext"
+import React from "react";
+import Todos from "./components/Todos";
+import AppContainer from "./components/AppContainer";
+import ModeSwitch from "./components/ModeSwitch";
+import ModeContextProvider from "./context/ModeContext";
 
 function App() {
   return (
     <ModeContextProvider>
       <AppContainer>
-    <div className="container my-4">
-      <h1 className="text-center">ToDos App</h1>
-      
-      <p className="mt-5">
-        Icons made by{" "}
-        <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
-          Freepik
-        </a>{" "}
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          {" "}
-          www.flaticon.com
-        </a>
-      </p>
-      <ModeSwitch />
-      <Todos />
-    </div>
+        <div className="container my-4">
+          <h1 className="text-center">ToDos App</h1>
 
-    </AppContainer>
+          <p className="mt-5">
+            Icons made by{" "}
+            <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+              Freepik
+            </a>{" "}
+            from{" "}
+            <a href="https://www.flaticon.com/" title="Flaticon">
+              {" "}
+              www.flaticon.com
+            </a>
+          </p>
+          <ModeSwitch />
+          <Todos />
+        </div>
+      </AppContainer>
     </ModeContextProvider>
-  )
+  );
 }
 
-export default App
+export default App;
+
+//maj
